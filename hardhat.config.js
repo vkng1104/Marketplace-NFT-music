e79312907ee13bc9a82963cf6a4996634c31f3d1
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-21 10:48:45 +0700

    chore: update dependencies and config files

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require("fs");
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 11155111,
+    },
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Marketplace from "./components/Marketplace";
+
+import Profile from "./components/Profile";
+import SellNFT from "./components/SellNFT";
+import NFTPage from "./components/NFTpage";
+
+function App() {
+  return (
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<Marketplace />} />
+        <Route path="/nftPage" element={<NFTPage />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sellNFT" element={<SellNFT />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
