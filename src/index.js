3834a510eba34105235a78a3c4e85c840225ddb0
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-22 14:55:57 +0700

    chore: remove redundant packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
